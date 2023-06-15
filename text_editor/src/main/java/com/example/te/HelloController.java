a43c3bc956468170cb4a51bf272e6e99ca9af948
Ashe-bin <118310102+Ashe-bin@users.noreply.github.com>
2023-06-16 02:53:28 +0300

    implimentation of edit

--- text_editor/src/main/java/com/example/te/HelloController.java
+++ text_editor/src/main/java/com/example/te/HelloController.java
@@ -91,6 +91,60 @@ public class HelloController {
         });
         fileLabel.setContextMenu(contextMenu1);
 
+        //Edit label menu items
+
+        MenuItem undoMenuItem = new MenuItem("Undo");
+        undoMenuItem.setOnAction(event -> undo());
+
+        MenuItem cutMenuItem = new MenuItem("Cut");
+        cutMenuItem.setOnAction(event -> cut());
+
+        MenuItem copyMenuItem = new MenuItem("Copy");
+        copyMenuItem.setOnAction(event -> copy());
+
+        MenuItem pasteMenuItem = new MenuItem("Paste");
+        pasteMenuItem.setOnAction(event -> paste());
+
+        MenuItem deleteMenuItem = new MenuItem("Delete");
+        deleteMenuItem.setOnAction(event -> delete());
+
+        MenuItem searchwithbingMenuItem = new MenuItem("Search With Google");
+        searchwithbingMenuItem.setOnAction(event -> searchwithbing());
+
+        MenuItem findMenuItem = new MenuItem("Find");
+        findMenuItem.setOnAction(event -> find());
+
+        MenuItem findnextMenuItem = new MenuItem("Find Next");
+        findnextMenuItem.setOnAction(event -> findnext());
+
+        MenuItem findpreveousMenuItem = new MenuItem("Find Previous");
+        findpreveousMenuItem.setOnAction(event -> findPrevious());
+
+        MenuItem replaceMenuItem = new MenuItem("Replace");
+        replaceMenuItem.setOnAction(event -> replace());
+
+        MenuItem gotoMenuItem = new MenuItem("Go To");
+        gotoMenuItem.setOnAction(event -> go_to());
+
+        MenuItem selectallMenuItem = new MenuItem("Select All");
+        selectallMenuItem.setOnAction(event -> selectall());
+
+        MenuItem timedateMenuItem = new MenuItem("Time/Date");
+        timedateMenuItem.setOnAction(event -> timedate());
+
+           //Edit Label context menu
+        ContextMenu contextMenu2 = new ContextMenu();
+        contextMenu2.getItems().addAll(undoMenuItem, deleteMenuItem, pasteMenuItem, copyMenuItem, cutMenuItem,
+                new SeparatorMenuItem(), replaceMenuItem, findpreveousMenuItem, findMenuItem, findnextMenuItem ,searchwithbingMenuItem,
+                new SeparatorMenuItem(),gotoMenuItem , selectallMenuItem,timedateMenuItem
+        );
+        editLabel.setOnMouseClicked(event ->{
+            if (event.getButton() == MouseButton.PRIMARY) {
+                contextMenu2.show(editLabel, event.getScreenX(), event.getScreenY());
+            }
+        });
+        editLabel.setContextMenu(contextMenu2);
+
 
     }
     // file functions
@@ -199,6 +253,148 @@ public class HelloController {
     public void exit() {
         System.exit(0);
     }
+
+    private void undo() {
+
+        textArea.undo();
+    }
+
+    private void cut() {
+        String selectedText = textArea.getSelectedText();
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent content = new ClipboardContent();
+        content.putString(selectedText);
+        clipboard.setContent(content);
+        textArea.replaceSelection("");
+    }
+
+    private void copy() {
+
+        String selectedText = textArea.getSelectedText();
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        ClipboardContent content = new ClipboardContent();
+        content.putString(selectedText);
+        clipboard.setContent(content);
+
+    }
+    private void paste() {
+        Clipboard clipboard = Clipboard.getSystemClipboard();
+        if (clipboard.hasString()) {
+            int caretPosition = textArea.getCaretPosition();
+            String clipboardContent = clipboard.getString();
+            textArea.insertText(caretPosition, clipboardContent);
+        }
+    }
+    private void delete() {
+
+        textArea.replaceSelection("");
+    }
+    private void searchwithbing() {
+        String searchQuery = textArea.getSelectedText();
+        if (!searchQuery.isEmpty()) {
+            try {
+                java.awt.Desktop.getDesktop().browse(new URI("https://www.google.com/search?q=" + searchQuery));
+            } catch (Exception e) {
+                showError("Error", "Failed to open the browser.");
+            }
+        }
+    }
+    private void find() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Find");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Enter text to find:");
+        Optional<String> result = dialog.showAndWait();
+
+        result.ifPresent(searchText -> {
+            String text = textArea.getText();
+            int startIndex = text.indexOf(searchText);
+            if (startIndex != -1) {
+                textArea.selectRange(startIndex, startIndex + searchText.length());
+            } else {
+                showError("Not Found", "Text not found.");
+            }
+        });
+    }
+
+    private void findnext() {
+        String searchText = textArea.getSelectedText();
+        int currentIndex = textArea.getCaretPosition();
+        int nextIndex = textArea.getText().indexOf(searchText, currentIndex + 1);
+        if (nextIndex != -1) {
+            textArea.selectRange(nextIndex, nextIndex + searchText.length());
+        } else {
+            showError("Not Found", "No more occurrences found.");
+        }
+    }
+
+    private void findPrevious() {
+        String searchText = textArea.getSelectedText();
+        if (searchText == null || searchText.isEmpty()) {
+            showError("No Selection", "Please select text to search for.");
+            return;
+        }
+
+        int currentIndex = textArea.getCaretPosition();
+        int previousIndex = textArea.getText().lastIndexOf(searchText, currentIndex - searchText.length() - 1);
+        if (previousIndex != -1) {
+            textArea.selectRange(previousIndex, previousIndex + searchText.length());
+        } else {
+            showError("Not Found", "No previous occurrences found.");
+        }
+    }
+
+    private void replace() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Replace");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Enter text to replace:");
+        Optional<String> result = dialog.showAndWait();
+
+        result.ifPresent(replacementText -> {
+            String text = textArea.getText();
+            int selectionStart = textArea.getSelection().getStart();
+            int selectionEnd = textArea.getSelection().getEnd();
+            String selectedText = text.substring(selectionStart, selectionEnd);
+            text = text.replace(selectedText, replacementText);
+            textArea.setText(text);
+        });
+    }
+    private void go_to() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Go To");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Enter line number:");
+        Optional<String> result = dialog.showAndWait();
+
+        result.ifPresent(lineNumberText -> {
+            try {
+                int lineNumber = Integer.parseInt(lineNumberText);
+                String[] lines = textArea.getText().split("\\n");
+                if (lineNumber >= 1 && lineNumber <= lines.length) {
+                    int position = 0;
+                    for (int i = 0; i < lineNumber - 1; i++) {
+                        position += lines[i].length() + 1; // +1 for the newline character
+                    }
+                    textArea.positionCaret(position);
+                } else {
+                    showError("Invalid Line Number", "Please enter a valid line number.");
+                }
+            } catch (NumberFormatException e) {
+                showError("Invalid Input", "Please enter a valid line number.");
+            }
+        });
+    }
+    private void selectall() {
+
+        textArea.selectAll();
+    }
+    private void timedate() {
+        LocalDateTime now = LocalDateTime.now();
+        String formattedDateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(now);
+        textArea.insertText(textArea.getCaretPosition(), formattedDateTime);
+    }
+
 }
 
 
